fa30a06a6b4fae1f24238b9fcbc1ff3bb2b6dcd8
Subalakshmi <ssubasekar@gmail.com>
2021-04-23 20:12:16 +0000

    adding datatypes file

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+#integer
+a = 4
+print(type(a))
+#float
+b=9.6
+print(type(b))
+#string
+c="abcd"
+print(type(c))
+#boolean
+t=8
+print(bool(t))
+u=0
+print(bool(u))
+#version is always declared as str
+x="2.3.4"
+print(type(x))
+
